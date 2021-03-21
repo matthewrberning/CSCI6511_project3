f43cc352a5787042e5c816c7f1c0781328303b39
Marshall Thompson <mdt_@gwu.edu>
2021-03-21 13:20:21 -0400

    add alpha beta pruning

--- minimax.py
+++ minimax.py
@@ -1,4 +1,5 @@
-def minimax(board, depth, maximizer, point=None):
+c = 0
+def minimax(board, depth, maximizer, point=None, alpha=float("-inf"), beta=float("inf")):
     """
     minimax algorithm to find most optimal move in a game of TTT.
     board- the game board you are playing on
@@ -6,6 +7,8 @@ def minimax(board, depth, maximizer, point=None):
     maximizer- if it is the maximizers turn
     point- should be none on initial call
     """
+    global c
+    c+=1
 
     # max depth reached, or the board is filled up
     if depth == 0 or board.isFull():
@@ -26,16 +29,24 @@ def minimax(board, depth, maximizer, point=None):
         value = float("-inf")
         for child in children:
             # value = int( max( value, minimax( board, depth - 1, not maximizer, (child[0], child[1]) ) ) )
-            value, point = my_max( value, minimax( board, depth - 1, not maximizer, (child[0], child[1]) )[0], point, tuple(child) )
+            value, point = my_max( value, minimax( board, depth - 1, not maximizer, (child[0], child[1]), alpha, beta )[0], point, tuple(child) )
+            alpha = max(value, alpha)
             board.remove_symbol( (child[0], child[1]) )
+
+            if beta <= alpha:
+                break
         return value, point
     # minimizer turn
     else:
         value = float("inf")
         for child in children:
             # value = int( min( value, minimax( board, depth - 1, not maximizer, (child[0], child[1]) ) ) )
-            value, point = my_min( value, minimax( board, depth - 1, not maximizer, (child[0], child[1]) )[0], point, tuple(child) )
+            value, point = my_min( value, minimax( board, depth - 1, not maximizer, (child[0], child[1]),alpha, beta )[0], point, tuple(child) )
+            beta = min(value, beta)
             board.remove_symbol( (child[0], child[1]) )
+
+            if beta <= alpha:
+                break
         return value, point
 
 
@@ -55,13 +66,12 @@ def my_min(value1, value2, point1, point2):
     else:
         return value1, point1
 
-# test code :)
-# b = Board(10,10)
+# test code :)'
+# from game import Board
+# b = Board(100,100)
 # symbol = 1
-# c = 0
 # while not b.isFull():
 #     b.add_symbol(minimax(b, 2, True)[1], 1 if symbol else -1)
-#     symbol = not symbol
-#     c+=1
+#     symbol = not symbol 
 #     print(b)
 # print(c)
